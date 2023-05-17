dac22cba87a032ca78890b09eb339572c0b4b60d
Jorik-Laptop <mastakcompany@gmail.com>
2023-05-17 16:56:02 +0700

    Corrected the comments

--- scripts.py
+++ scripts.py
@@ -1,39 +1,34 @@
 import random
-import django
 
 
 from datacenter.models import Schoolkid
-from datacenter.models import Teacher
 from datacenter.models import Lesson
 from datacenter.models import Subject
 from datacenter.models import Commendation
 from datacenter.models import Mark
 from datacenter.models import Chastisement
 
+COMMENDATIONS = ['Молодец!', 'Отлично!', 'Хорошо!', 'Великолепно!',
+                 'Прекрасно!', 'Талантливо!', 'Потрясающе!']
+
 
 def fix_marks(schoolkid):
-    marks = Mark.objects.filter(schoolkid=schoolkid, points__in=["2", "3"])
-    for mark in marks:
-        mark.points = "5"
-        mark.save()
+    schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid).first()
+    Mark.objects.filter(schoolkid=schoolkid, points__in=["2", "3"]).update(points="5")
 
 
 def remove_chastisements(schoolkid):
-    chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
-    for chastisement in chastisements:
-        chastisement.delete()
+    Chastisement.objects.filter(schoolkid=schoolkid).delete()
 
 
 def create_commendation(name, subject):
 
-    commendations = ['Молодец!', 'Отлично!', 'Хорошо!', 'Великолепно!',
-                     'Прекрасно!', 'Талантливо!', 'Потрясающе!']
     try:
         child = Schoolkid.objects.get(full_name__contains=name).first()
-    except django.core.exceptions.MultipleObjectsReturned:
+    except Schoolkid.DoesNotExist:
         print(f"Several child with the name {name} have been found!\n"
               f"Please specify who you mean.")
-    except django.core.exceptions.ObjectDoesNotExist:
+    except Schoolkid.MultipleObjectsReturned:
         print(f"A child named {name} was not found!")
     except Exception as exp:
         print(f"Error: {exp}")
@@ -48,12 +43,10 @@ def create_commendation(name, subject):
             subject=subject
         ).order_by('?').first()
 
-        teacher = Teacher.objects.all().order_by('?').first()
-
         Commendation.objects.create(
-            text=random.choice(commendations),
+            text=random.choice(COMMENDATIONS),
             created=lesson.date,
             schoolkid=child,
             subject=subject,
-            teacher=teacher
+            teacher=lesson.teacher
         )
